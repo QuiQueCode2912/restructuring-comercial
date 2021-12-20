87db4e3ee04e07f411437b2a1da2f14cbd344ba4
Vivianne Guzman <viviannegw@gmail.com>
2021-12-20 10:52:11 -0500

    UPD: Respuesta de Holidays
    Respuesta automática de fuera de oficina por holidays

--- resources/views/components/request-step-4.blade.php
+++ resources/views/components/request-step-4.blade.php
@@ -4,14 +4,16 @@
       <div class="col-12 col-md-10">
         <i class="fe fe-check round text-white bg-success" style="background-color:#63AF3F !important"></i>
       </div>
-      <p style="font-size:20px; margin:0; font-weight:600; color:#63AF3F !important" class="text-success text-center">Gracias por completar tus datos</p>
-      <p class="text-center" style="margin:0 0 40px; font-family:'Trola', sans-serif; font-size:18px">En unos minutos recibirás un correo con la información ingresada.</p>
+      <p style="font-size:20px; margin:0; font-weight:600; color:#63AF3F !important" class="text-success text-center">Muchas gracias por solicitar los servicios comerciales de Ciudad del Saber.</p>
+      <p class="text-center" style="margin:0 0 40px; font-family:'Trola', sans-serif; font-size:18px">Nuestros espacios de eventos y oficinas administrativas se encuentran cerradas hasta el lunes 3 de enero.</p>
     </div>
     <div class="row">
       <div class="col-12">
         <p style="font-size:24px; font-family:'Trola', sans-serif; font-weight:600" class="text-center">
-        Un miembro del equipo de la Ciudad de Saber se pondrá en contacto 
-        contigo para ofrecerte la mejor propuesta para tu solicitud.
+        Las solicitudes entrantes se procesarán en orden de llegada por nuestros ejecutivos de venta a partir de esta fecha, 
+		por lo que nos disculpamos de antemano por cualquier demora que pueda experimentar en la respuesta de su servicio.
+        Gracias por su comprensión y ¡Felices Fiestas!
+
         </p>
       </div>
     </div>
