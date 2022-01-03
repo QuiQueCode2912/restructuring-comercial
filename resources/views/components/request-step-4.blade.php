4734765e27b223d99620620d21abbc8e84837c03
Vivianne Guzman <viviannegw@gmail.com>
2022-01-03 10:28:10 -0500

    UPD: Cambio de respuesta automatica
    Se cambio el mensaje que se muestra al llena el formulario de registro.

--- resources/views/components/request-step-4.blade.php
+++ resources/views/components/request-step-4.blade.php
@@ -5,15 +5,13 @@
         <i class="fe fe-check round text-white bg-success" style="background-color:#63AF3F !important"></i>
       </div>
       <p style="font-size:20px; margin:0; font-weight:600; color:#63AF3F !important" class="text-success text-center">Muchas gracias por solicitar los servicios comerciales de Ciudad del Saber.</p>
-      <p class="text-center" style="margin:0 0 40px; font-family:'Trola', sans-serif; font-size:18px">Nuestros espacios de eventos y oficinas administrativas se encuentran cerradas hasta el lunes 3 de enero.</p>
+      <p class="text-center" style="margin:0 0 40px; font-family:'Trola', sans-serif; font-size:18px">Hemos recibido su solicitud y le haremos llegar su cotización en un período máximo de 24 horas laborables.</p>
     </div>
     <div class="row">
       <div class="col-12">
         <p style="font-size:24px; font-family:'Trola', sans-serif; font-weight:600" class="text-center">
-        Las solicitudes entrantes se procesarán en orden de llegada por nuestros ejecutivos de venta a partir de esta fecha, 
-		por lo que nos disculpamos de antemano por cualquier demora que pueda experimentar en la respuesta de su servicio.
-        Gracias por su comprensión y ¡Felices Fiestas!
-
+        Le agradecemos revisar que sus datos de contacto estén correctos para que uno de nuestros ejecutivos de venta le contacte si necesita aclarar algún punto de su solicitud. 	
+	    Les recordamos que nuestro horario de atención es de lunes a viernes de 8:00 a.m. a 5:00 p.m.
         </p>
       </div>
     </div>
