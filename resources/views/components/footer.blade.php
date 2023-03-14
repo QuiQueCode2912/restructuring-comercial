42da7c4562e9bdf65f8641a62a0ceb685b081323
José Ureña <jurena@reddevilstudios.com>
2023-03-14 16:53:09 -0500

    Newsletter

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
   <div class="container">
     <div class="row">
-      <div class="col-12 col-md-4">
+      <div class="col-12 col-md-5">
         <a href="#" class="logo"><img src="/assets/images/logo-white-blue.png" /></a>
         <p>
           <i class="fe fe-smartphone"></i>
@@ -16,7 +16,21 @@
           Apartado postal 0843 - 03081.
         </p>
       </div>
-      <div class="col-md-8 col-12" id="twitter-timeline"></div>
+      <div class="col-12 col-md-4">
+      <h3 color="#000"><b>Suscríbete</b></h3>
+      <h4>a nuestro boletín</h4>
+      <form class="js-cm-form" id="subForm" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="5B5E7037DA78A748374AD499497E309E623FDB3E773F80FD29AEED6589C4954FF040280997B0D8EBFCB912E0396284DF35F0CB7A51A5A8CFB6287E3753BD20E0">
+      <input class="form-control" aria-label="Nombre y apellido" id="fieldName" maxlength="200" name="cm-name" placeholder="Nombre" type="text">
+      <input class="form-control" autocomplete="Email" aria-label="Correo electrónico" class="js-cm-email-input qa-input-email" id="fieldEmail" maxlength="200" name="cm-bhylljl-bhylljl" placeholder="Dirección de email" required="" type="email" style="margin-top:12px">
+      <input type="hidden"  aria-label="URL" id="fieldddtryhhi" maxlength="200" name="cm-f-ddtryhhi" value="<?php echo URL::current(); ?>">
+      <button class="btn btn-primary" type="submit" style="margin-top:12px">Subscribirme</button>
+      </form>
+      <script type="text/javascript" src="https://js.createsend1.com/javascript/copypastesubscribeformlogic.js"></script>
+      </div>
+      <div class="col-12 col-md-3">
+      <img src="/assets/images/newsletter.png"/>
+      </div>
+     <!-- <div class="col-md-8 col-12" id="twitter-timeline"></div>-->
     </div>
   </div>
 </footer>
