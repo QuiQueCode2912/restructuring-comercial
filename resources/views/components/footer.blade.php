a07983a8a447d8a239404c98be82c6ab2f11c320
José Ureña <jurena@reddevilstudios.com>
2023-04-10 09:11:39 -0500

    Fix newsletter

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -21,7 +21,7 @@
       <h4>a nuestro boletín</h4>
       <form class="js-cm-form" id="subForm" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="5B5E7037DA78A748374AD499497E309E623FDB3E773F80FD29AEED6589C4954FF040280997B0D8EBFCB912E0396284DF35F0CB7A51A5A8CFB6287E3753BD20E0">
       <input class="form-control" aria-label="Nombre y apellido" id="fieldName" maxlength="200" name="cm-name" placeholder="Nombre" type="text">
-      <input class="form-control" autocomplete="Email" aria-label="Correo electrónico" class="js-cm-email-input qa-input-email" id="fieldEmail" maxlength="200" name="cm-bhylljl-bhylljl" placeholder="Dirección de email" required="" type="email" style="margin-top:12px">
+      <input class="form-control js-cm-email-input qa-input-email" autocomplete="Email" aria-label="Correo electrónico" class="js-cm-email-input qa-input-email" id="fieldEmail" maxlength="200" name="cm-bhylljl-bhylljl" placeholder="Dirección de email" required="" type="email" style="margin-top:12px">
       <input type="hidden"  aria-label="URL" id="fieldddtryhhi" maxlength="200" name="cm-f-ddtryhhi" value="<?php echo URL::current(); ?>">
       <button class="btn btn-primary" type="submit" style="margin-top:12px">Subscribirme</button>
       </form>
