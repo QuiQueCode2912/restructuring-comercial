324dda5c6d739d0c1a45be6cc8760e7f272f6b06
José Ureña <jurena@reddevilstudios.com>
2023-05-23 12:31:51 -0500

    Fix final de mensajes desde Salesforce a Laravel

--- app/Http/Controllers/eventsController.php
+++ app/Http/Controllers/eventsController.php
@@ -12,7 +12,7 @@ class eventsController extends Controller
 {
     public function handleInboundMessage(Request $request)
     {
-        Log::info('Llamada a handleInboundMessage');
+     //   Log::info('Llamada a handleInboundMessage');
 
         // Reemplaza esto con la API token que compartes con Salesforce
         $sharedApiToken = '906F00000008zQWIAY';
@@ -20,13 +20,13 @@ class eventsController extends Controller
         // Verifica si la API token está presente en la solicitud
         $apiToken = $request->query('tk');
         if ($apiToken !== $sharedApiToken) {
-            Log::info('No autorizado - Token incorrecto');
+    //        Log::info('No autorizado - Token incorrecto');
             return response('Unauthorized', 401);
         }
 
         // Carga el contenido del mensaje SOAP/XML
         $xmlContent = $request->getContent();
-        Log::info('Contenido del Request: ' . $xmlContent);
+    //    Log::info('Contenido del Request: ' . $xmlContent);
         $xmlContent = str_replace('xmlns:soapenv', 'xmlnssoapenv', $xmlContent);
         $xmlContent = str_replace('xsi:type', 'xsitype', $xmlContent);
         $xmlContent = str_replace('xmlns:sf', 'xmlnssf', $xmlContent);
@@ -38,11 +38,18 @@ class eventsController extends Controller
         $xml = simplexml_load_string($xmlContent);
         $json = json_encode($xml);
         $data = json_decode($json, true);
-        Log::info('Data: ' . $json);
+    //    Log::info('Data: ' . $json);
         //print_r($data);
-        if (isset($data['soapenv:Body']['notifications']['Notification']['sObject'])) {
-            $sObject = $data['soapenv:Body']['notifications']['Notification']['sObject'];
+        if (isset($data['soapenv:Body']['notifications']['Notification'])) {
+            $notifications = $data['soapenv:Body']['notifications']['Notification'];
+    
+            // Verifica si las notificaciones es una matriz. Si no es una matriz, conviértelo en una matriz con un solo elemento.
+            if (isset($notifications['sObject'])) {
+                $notifications = [$notifications];
+            }
 
+            foreach ($notifications as $notification) {
+                $sObject = $notification['sObject'];
             // Extrae los campos deseados del objeto Event
             $id = (string)$sObject['sf:Id'];
             $asunto = (string)$sObject['sf:Asunto__c'];
@@ -51,7 +58,7 @@ class eventsController extends Controller
             $startDateTime = (string)$sObject['sf:StartDateTime'];
             $endDateTime = (string)$sObject['sf:EndDateTime'];
             $ownerId = (string)$sObject['sf:CreatedById'];
-        }
+        
         // Crea una nueva instancia del modelo Event y asigna los valores de los campos
         $event = Event::firstOrNew(['sfId' => $id]);
         $event->subject = $asunto;
@@ -64,9 +71,10 @@ class eventsController extends Controller
 
         // Guarda el evento en la base de datos
         $event->save();
-
+            }
+        }
         // Registra un mensaje en el log para fines de depuración
-        Log::info("Evento procesado: {$id}");
+   //     Log::info("Evento procesado: {$id}");
 
         // Crear la respuesta XML
         $responseContent = '<?xml version="1.0" encoding="UTF-8"?>';
@@ -81,7 +89,7 @@ class eventsController extends Controller
         ob_start();
         ob_end_clean();
         // Enviar la respuesta XML
-        Log::info('Contenido de la Respuesta: ' . $responseContent);
+    //    Log::info('Contenido de la Respuesta: ' . $responseContent);
         return response($responseContent, Response::HTTP_OK)
             ->header('Content-Type', 'text/xml');
     }
