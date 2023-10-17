c94ddab20b12d060a808802a522710bef0e886d7
agus4022011 <59782029+agus4022011@users.noreply.github.com>
2023-10-17 20:01:55 -0300

    Update IndexController.php

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1886,7 +1886,7 @@ class IndexController extends Controller
             } else {
                 if (substr($request->token, 0, 3) != '001') {
                     $query = "SELECT 
-                        Id, Precio_Estimado__c, Pago_confirmado__c, Espacios_que_desea_reservar__c,WebSessionId__c
+                        Id, Precio_Estimado__c, Pago_confirmado__c, Espacios_que_desea_reservar__c,WebSessionId__c,VenueID__c
                         FROM Lead	
                         WHERE Id = '{$request->token}'";
 
@@ -1897,7 +1897,16 @@ class IndexController extends Controller
 
                         if (isset($data['Estado']) && substr($data['Estado'], 0, 6) == 'Aproba') {
                             $lead_id = $lead['records'][0]['Id'];
-                            $concepto = nl2br($lead['records'][0]['Espacios_que_desea_reservar__c']);
+
+                            $concepto = '';
+                            if(isset($lead['records'][0]['VenueID__c']) ){
+                                if($lead['records'][0]['VenueID__c'] == '02i3m00000D9BANAA3'){
+                                $concepto ='';
+                                }
+                                else{
+                                    $concepto = nl2br($lead['records'][0]['Espacios_que_desea_reservar__c']);
+                                }
+                            }
 
                             //      $receiptData = ['Confirmado__c' => false, 'Lead__c' => $request->token, 'Monto__c' => $data['TotalPagado'], 'Soporte__c' => $uploaded_file, 'Numero_de_transaccion__c' => $data['Oper'], 'Fecha_de_pago__c' => $date->format('Y-m-d\TH:i:s.000\Z') , 'Tipo__c' => (isset($data['method']) ? $data['method'] : 'Páguelo Fácil') ];
 
