c9856bfdf12eda22432ac33c17aedde55b9ab4db
Agustin Farias <59782029+agus4022011@users.noreply.github.com>
2023-12-03 11:46:14 -0300

    Upload testing sandbox

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,3 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::get('/testing',[SalesforceController::class, 'testConection']);
