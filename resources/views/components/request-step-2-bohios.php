107eb1d528455981ba5667e0664ad6609bac5258
agustin.farias <agustin.farias>
2024-03-13 16:10:03 -0300

    Describe tu evento

--- resources/views/components/request-step-2-bohios.php
+++ resources/views/components/request-step-2-bohios.php
@@ -134,10 +134,9 @@ if (is_null($to_hour)) {
         <?php endif ?>
         <div class="row">
             <div class="col-12">
-                <div class="form-group ">
-                    <label for="description" class="font-weight-bold" style="font-size: 1rem !important; " > <span style="color: #0088ff;">*</span><small> Describe tu evento</small></label>
-                    <p class="small" style="font-size: 0.7rem !important;">Compartenos la agenda de tu evento y en caso de solicitar canchas, sillas, mesas y/o bohios adicionales añade aquí las especificaciones sobre estos elementos adicionales.</p>
-                    <textarea name="description" required id="description"><?php echo session()->get('description', old('description')); ?></textarea>
+                <p class="small" style="font-size: 0.7rem !important;">Compartenos la agenda de tu evento y en caso de solicitar canchas, sillas, mesas y/o bohios adicionales añade aquí las especificaciones sobre estos elementos adicionales.</p>
+                <div class="form-group required ">
+                    <textarea name="description" placeholder="Describe tu evento" required id="description"><?php echo session()->get('description', old('description')); ?></textarea>
                 </div>
             </div>
         </div>
